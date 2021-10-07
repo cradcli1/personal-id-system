099f2b593d2da215781cfc7050d51bdbc3831ff1
Christopher Radcliffe <christopherradcliffe@Christophers-MacBook-Pro-3.local>
2021-10-07 10:30:16 -0400

    final hooks

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from dashboardReceivers import getDashboardData
 from messageFormatting import formatWhoIsAround
 from slackWebhooks import sendMessage
+
+APIHOOK = "https://hooks.slack.com/services/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"
 dashboardAPIResponce = getDashboardData("mock", 0, 0, True)
 print("\nDataReceived\n-----------------------------------\n" +
       str(dashboardAPIResponce))
 if dashboardAPIResponce:
     message = formatWhoIsAround(dashboardAPIResponce)
-    print("\n\nMessage: \n" + message)
+    print("\n\nMessage: \n" + message + "\n")
+    print("Sent To:\n" + APIHOOK)
     slackMessage = [{
-        "API": "https://hooks.slack.com/services/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",
+        "API": APIHOOK,
         "message": message
     }]
     sendMessage(slackMessage)
