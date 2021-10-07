4c49088988c775249bbddf2fa472f0ea8de4278e
Christopher Radcliffe <christopherradcliffe@christohersmbp3.myfiosgateway.com>
2021-10-06 21:23:26 -0400

    Demo set working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dashboardReceivers import getDashboardData
 from messageFormatting import formatWhoIsAround
+from slackWebhooks import sendMessage
 dashboardAPIResponce = getDashboardData("mock", 0, 0, True)
 
 if not dashboardAPIResponce:
@@ -9,3 +10,9 @@ else:
 
 message = formatWhoIsAround(dashboardAPIResponce)
 print("\n\nMessage: \n" + message)
+
+slackMessage = [{
+    "API": "https://hooks.slack.com/services/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",
+    "message": message
+}]
+sendMessage(slackMessage)
