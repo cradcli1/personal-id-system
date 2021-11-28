b7343a63381e773390802e1e7bc326b65d3b5898
Christopher Radcliffe <christopherradcliffe@christohersmbp3.myfiosgateway.com>
2021-11-28 12:17:08 -0500

    Adding running every minute

--- app.py
+++ app.py
@@ -4,35 +4,43 @@ from messageFormatting import formatWhoIsAround
 from slackWebhooks import sendMessage
 from getData import getData
 from array import *
+import schedule
+import time
 
 datastore = [{"name": "Christopher Radcliffe",
               "lastState": "", "whereToSend": ""}]
 APIHOOK = "https://hooks.slack.com/services/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"
 
-APIResponce = getData("mock", 0, 0, True)
-#print("API RESPONCE\n" + str(APIResponce) + "\n")
-#print("datastore\n" + str(datastore) + "\n")
 
-if APIResponce:
-    messages = []
-    for blindUser in datastore:
-        givenUserResponce = {}
-        # print(blindUser)
-        room = APIResponce[blindUser["name"]]["roomID"]
-        # print(room)
-        for people in APIResponce:
-            # print(APIResponce[people])
-            # print(APIResponce[people]["roomID"])
-            if APIResponce[people]["roomID"] == room and people != blindUser["name"]:
-                givenUserResponce[people] = APIResponce[people]
-            print(str(givenUserResponce) + "\n\n\n")
-    print(formatWhoIsAround(APIResponce))
-    messages.append(
-        {formatWhoIsAround(APIResponce), blindUser["whereToSend"]})
-    try:
-        print("SENDING MESSAGE :" + str(messages))
-        # sendMessage(givenUserResponce)
-    except:
-        print("STUFF")
-else:
-    print("Stuff")
+def main():
+    APIResponce = getData("mock", 0, 0, True)
+    if APIResponce:
+        messages = []
+        for blindUser in datastore:
+            givenUserResponce = {}
+            # print(blindUser)
+            room = APIResponce[blindUser["name"]]["roomID"]
+            # print(room)
+            for people in APIResponce:
+                print(people != blindUser["name"])
+                if APIResponce[people]["roomID"] == room and people != blindUser["name"]:
+                    givenUserResponce[people] = APIResponce[people]
+            formatedUserResponce = formatWhoIsAround(givenUserResponce)
+            print(formatedUserResponce)
+            print(blindUser["lastState"])
+            if blindUser["lastState"] != formatedUserResponce:
+                messages.append(
+                    {formatedUserResponce, blindUser["whereToSend"]})
+                blindUser["lastState"] = formatedUserResponce
+        try:
+            print("SENDING MESSAGE :" + str(messages))
+            # sendMessage(givenUserResponce)
+        except:
+            print("STUFF")
+    else:
+        print("Stuff")
+
+schedule.every(1).minutes.do(main)
+main()
+while True:
+    schedule.run_pending()
